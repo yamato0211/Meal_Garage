9f91f1207dd26a7123f2145131bff0d7dca9c8c8
PigeonsHouse <bullbull1230@gmail.com>
2022-06-18 11:38:05 +0900

    [add] API定義書の追加

--- db/model.py
+++ db/model.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column
+from sqlalchemy import Column, Date, ForeignKey, Integer
 from sqlalchemy.sql.sqltypes import String
 from uuid import uuid4
 
@@ -16,3 +16,17 @@ class User(Base):
     name = Column(String)
     email = Column(String, unique=True)
     password_hash = Column(String, nullable=True)
+
+
+class Food(Base):
+    __tablename__ = 'foods'
+    food_id = Column(String, primary_key=True, default=generate_uuid)
+    name = Column(String)
+
+
+class Garage(Base):
+    __tablename__ = 'garages'
+    user_id = Column(String, ForeignKey('users.user_id'))
+    food_id = Column(String, ForeignKey('foods.food_id'))
+    quantity = Column(Integer)
+    limit_at = Column(Date)
